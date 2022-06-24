b1c6592ddef2b7dfc0e7da3fc605920235f3f4cb
Shubham Badure <badureshubham1997@gmail.com>
2022-06-24 20:35:33 +0530

    initial commit

---
+++ long_word.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	fmt.Scanln(&n)
+	var names = make([]string, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanln(&names[i])
+	}
+	//for _, str := range names {
+	for i := 0; i < n; i++ {
+		if len(names[i]) > 10 && len(names[i]) < 100 {
+			fmt.Printf("INVALID")
+		} else {
+			fmt.Printf("%s\n", names[i])
+		}
+
+	}
+}
