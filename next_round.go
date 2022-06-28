36722ad755882f9e3e8ec1c70daf49134786e628
Shubham Badure <badureshubham1997@gmail.com>
2022-06-28 18:40:41 +0530

    syntax edit

--- next_round.go
+++ next_round.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	var n, k, f int
-	fmt.Scanln(&n, &k)
+	fmt.Scan(&n, &k)
 	var round = make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scanln(&round[i])
+		fmt.Scan(&round[i])
 	}
 	score := round[k-1]
 	for j := 0; j < n; j++ {
