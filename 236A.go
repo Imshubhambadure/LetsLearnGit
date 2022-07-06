828a25cfd5956f508d54a854916e7046d0cbc5ad
GAniket_03 <aniketghotekar@gmail.com>
2022-07-06 23:34:20 +0530

    added the unique slice concept

--- 236A.go
+++ 236A.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	var a string
 	fmt.Scan(&a)
 	//str := make([]string, 0)
-	var count = 1
-	for i := 0; i < len(a)-1; i++ {
-		if a[i] != a[i+1] {
-			count++
-		}
+	b := strings.Split(a, "")
+	// fmt.Println(len(b))
+	// var count = 1
+	// for i := 0; i < len(a)-1; i++ {
+	// 	if a[i] != a[i+1] {
+	// 		count++
+	// 	}
 
+	// }
+	var ds = make(map[string]bool)
+	var unique_ls []string
+	for _, val := range b {
+		if _, ds_val := ds[val]; !ds_val {
+			ds[val] = true
+			unique_ls = append(unique_ls, val)
+		}
 	}
-	if count%2 == 0 {
+	if len(unique_ls)%2 == 0 {
 		fmt.Println("CHAT WITH HER!")
 	} else {
 		fmt.Println("IGNORE HIM!")
